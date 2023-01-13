59893ae0abe0b0c3f67547a8f8befd1ec93f2322
Ariel Aballay <arielaballay7@gmail.com>
2023-01-13 03:33:16 -0300

    main

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("icon__menu").addEventListener("click", mostrar_menu);
+
+function mostrar_menu(){
+
+    document.querySelector(".menu").classList.toggle("mostrar_menu");
+}
+
